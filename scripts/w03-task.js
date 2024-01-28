57edb629b9a6ac79e7955516aebe648fba613f48
ZigzagAlloy01 <ivanblued@gmail.com>
2024-01-28 01:00:38 -0700

    Week 3 activity, calculations

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -1,12 +1,8 @@
 /* LESSON 3 - Programming Tasks */
-let addNumber1 = Number(document.querySelector('#add1').value);
-let addNumber2 = Number(document.querySelector('#add2').value);
-let substractNumber1 = Number(document.querySelector('#subtract1').value);
-let substractNumber2 = Number(document.querySelector('#subtract2').value);
-let multiplyNumber1 = Number(document.querySelector('#factor1').value);
-let multiplyNumber2 = Number(document.querySelector('#factor2').value);
-let dividend = Number(document.querySelector('#dividend').value);
-let divisor = Number(document.querySelector('#divisor').value);
+
+
+
+
 let subtotalInput = document.getElementById("subtotal");
 let memberCheckbox = document.getElementById("member");
 let getTotalButton = document.getElementById("getTotal");
@@ -18,8 +14,10 @@ function add(number1, number2) {
     return number1 + number2;
 }
 
-function addNumbers(one, two) {
-    return document.querySelector('#sum').innerHTML = add(one, two);;
+function addNumbers() {
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+    return document.querySelector('#sum').value = add(addNumber1, addNumber2);
 }
 
 /* Function Expression - Subtract Numbers */
@@ -28,8 +26,10 @@ function subtract(number1, number2) {
     return number1 - number2;
 }
 
-function subtractNumbers(one, two) {
-    return document.querySelector('#difference').value = subtract(one, two); 
+function subtractNumbers() {
+    let substractNumber1 = Number(document.querySelector('#subtract1').value);
+    let substractNumber2 = Number(document.querySelector('#subtract2').value);
+    return document.querySelector('#difference').value = subtract(substractNumber1, substractNumber2); 
 }
 
 /* Arrow Function - Multiply Numbers */
@@ -38,8 +38,10 @@ function multiply(number1, number2) {
     return (number1 * number2);
 }
 
-function multiplyNumbers(one, two) {
-    return document.querySelector('#product').value = multiply(one, two); 
+function multiplyNumbers() {
+    let multiplyNumber1 = Number(document.querySelector('#factor1').value);
+    let multiplyNumber2 = Number(document.querySelector('#factor2').value);
+    return document.querySelector('#product').value = multiply(multiplyNumber1, multiplyNumber2); 
 }
 
 /* Open Function Use - Divide Numbers */
@@ -48,22 +50,23 @@ function divide(number1, number2) {
     return (number1 / number2);
 }
 
-function divideNumbers(one, two) {
-    return document.querySelector('#quotient').value = divide(one, two); 
+function divideNumbers() {
+    let dividend = Number(document.querySelector('#dividend').value);
+    let divisor = Number(document.querySelector('#divisor').value);
+    return document.querySelector('#quotient').value = divide(dividend, divisor); 
 }
 
 /* Decision Structure */
 
-document.querySelector('#addNumbers').addEventListener('click', addNumbers(addNumber1, addNumber2));
-document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers(substractNumber1, substractNumber2));
-document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers(multiplyNumber1, multiplyNumber2));
-document.querySelector('#divideNumbers').addEventListener('click', divideNumbers(dividend, divisor));
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
+document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
+document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
 
 //totalSpan.textContent = `$${subtotal.toFixed(2)}`;
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
 let listOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-let listOfMultiplication = [];
 /* Output Odds Only Array */
 function oddNumbers(list) {
     return list.filter(element => element % 2 === 0);
@@ -74,23 +77,18 @@ function evenNumbers(list) {
 }
 /* Output Sum of Org. Array */
 function sumOfArray(list) {
-    for (let i = 0; i < list.lenght; i++) {
-        sum += list[i];
-    }
+    let sum = 0;
+    list.map(element => sum += element);
     return sum;
 }
 /* Output Multiplied by 2 Array */
 function multiplied(list) {
-    for (let i = 0; i < list.lenght; i++) {
-        listOfMultiplication.push(list[i] * 2);
-    }
-    return listOfMultiplication;
+    return list.map(number => number * 2);
 }
 /* Output Sum of Multiplied by 2 Array */
 function sumOfMultiplied(list) {
-    for (let i = 0; i < list.lenght; i++) {
-        sum += list[i];
-    }
+    let sum = 0;
+    list.map(element => sum += (element+element));
     return sum;
 }
 
@@ -99,4 +97,4 @@ document.querySelector('#odds').innerHTML = oddNumbers(listOfNumbers);
 document.querySelector('#evens').innerHTML = evenNumbers(listOfNumbers);
 document.querySelector('#sumOfArray').innerHTML = sumOfArray(listOfNumbers);
 document.querySelector('#multiplied').innerHTML = multiplied(listOfNumbers);
-document.querySelector('#sumOfMultiplied').innerHTML = sumOfMultiplied(listOfMultiplication);
+document.querySelector('#sumOfMultiplied').innerHTML = sumOfMultiplied(listOfNumbers);
