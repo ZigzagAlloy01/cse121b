18eef86378514e9169f22f629d1e31aec97b2534
ZigzagAlloy01 <ivanblued@gmail.com>
2024-02-10 22:34:07 -0700

    W5 Task completed

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -6,7 +6,8 @@ const templeList = [];
 
 /* async displayTemples Function */
 const displayTemples = (temples) => {
-    templeList.forEach(function(temple) {
+    reset();
+    temples.forEach(function(temple) {
         let article = document.createElement('article');
         let h3 = document.createElement('h3');
         h3.textContent = temple.templeName;
@@ -42,7 +43,29 @@ const reset = function() {
 
 /* filterTemples Function */
 
+const sortBy = (temples) => {
+    reset();
+    let filter = document.querySelector('#filtered').value;
+    switch(filter) {
+        case 'utah':
+            displayTemples(temples.filter(temple => temple.location.includes('Utah')));
+            break;
+        case 'nonutah':
+            displayTemples(temples.filter(temple => !temple.location.includes('Utah')));
+            break;
+        case 'older':
+            displayTemples(temples.filter(temple => new Date(temple.dedicated) < new Date(1950, 0, 1)));
+            break;
+        case 'all':
+        default:
+            displayTemples(temples);
+    }
+};
 /* Event Listener */
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#filtered').addEventListener('change', () => {
+        sortBy(templeList);
+    });
+});
+
 getTemples();
-displayTemples();
-reset();
